a539a41830b0e3c6f5f59d4f3d6adbc0b8dfc337
Massimo Triassi <massimo@plankdesign.com>
2020-08-04 11:32:55 -0400

    make models publishable

--- src/VersionableServiceProvider.php
+++ src/VersionableServiceProvider.php
@@ -23,13 +23,16 @@ class VersionableServiceProvider extends ServiceProvider
 
         if ($this->app->runningInConsole()) {
 
-            $this->commands([
-                StartRevisioning::class,
-            ]);
-
+            // Publish configuration files
             $this->publishes([
                 __DIR__.'/../config/checkpoint.php' => config_path('checkpoint.php'),
             ], 'config');
+
+            // Publish extendable models
+            $this->publishes([
+                __DIR__.'/../src/Models/Checkpoint.php' => base_path('app/Checkpoint.php'),
+                __DIR__.'/../src/Models/Revision.php' => base_path('app/Revision.php'),
+            ], 'models');
 
             // Publishing the views.
             /*$this->publishes([
@@ -47,7 +50,9 @@ class VersionableServiceProvider extends ServiceProvider
             ], 'lang');*/
 
             // Registering package commands.
-            // $this->commands([]);
+            $this->commands([
+                StartRevisioning::class,
+            ]);
         }
 
 
